179a6ff89ce04a467c3be0066e435ce743bb162f
Manuel <cm039398@gmail.com>
2024-03-13 03:21:26 -0600

    hotfix en actualizarr categoria

--- controllers/categoriasController.js
+++ controllers/categoriasController.js
@@ -13,7 +13,7 @@ exports.crearCategorias = async (req, res) => {
 }
 exports.obtenerCategoriasDelEstudiante= async (req, res) => {
     try {
-        const categoria = await Categorias.find({estudiante: req.body.id})
+        const categoria = await Categorias.find({estudiante: req.params.estudiante})
         res.json(categoria);
     } catch (error) {
         console.log(error);
@@ -39,17 +39,19 @@ exports.obtenerCategoria = async (req, res) => {
 
 exports.actualizarCategorias = async (req, res) => {
     try {
-        const {id, nombre, descripcion, estudiante} = req.body;
+        const nombre = req.body.nombre, descripcion = req.body.descripcion , estudiante =req.body.estudiante 
         let categorias = await Categorias.findById(req.params.id);
-
+        
         if (!categorias) {
             res.status(404).json({msg: "La categoria no existe"});
-        }
-        
-        categorias.id = id;
+        }        
+        // categorias.id = id;
         categorias.nombre = nombre;
         categorias.descripcion = descripcion;
         categorias.estudiante = estudiante;
+        categorias.save();
+        res.json({msg: "La categoria se ha actualizado"});
+        console.log(categorias);
     } catch (error) {
         console.log(error);
         res.status(500).send(error);    
--- routes/categoriasRoutes.js
+++ routes/categoriasRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const categoriasController = require('../controllers/categoriasController');
 
 router.post('/', categoriasController.crearCategorias);
-router.get('/:estudiante', categoriasController.obtenerCategoriasDelEstudiante);
+router.get('/porEstudiante/:estudiante', categoriasController.obtenerCategoriasDelEstudiante);
 router.get('/:id', categoriasController.obtenerCategoria);
 router.put('/:id/', categoriasController.actualizarCategorias); 
 router.delete('/:id/', categoriasController.eliminarCategorias);
